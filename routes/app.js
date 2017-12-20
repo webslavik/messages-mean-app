147f40f6192fb1a401d15537458b0f320e669514
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-12-20 12:25:32 +0200

    NodeJS basic

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/message/:msg', function(req, res, next) {
+    res.render('node', {message: req.params.msg});
+});
+
+router.get('/message', function(req, res, next) {
+    var message = req.body.test;
+    res.redirect('/message/' + message);
+});
+
 module.exports = router;
