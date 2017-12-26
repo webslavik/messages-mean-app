898b7fb776a650042866c40b666d72d71c67b22d
Slavik Sukhanov <slavik.sukhanov@gmail.com>
2017-12-26 21:30:15 +0200

    added Message and User models, added markup

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html'
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styles: [`
+    .author {
+      display: inline-block;
+      width: 80%;
+      font-style: italic;
+      font-size: 12px;
+    }
+    .config {
+      display: inline-block;
+      width: 19%;
+      text-align: right;
+      font-size: 12px;
+    }
+  `]
 })
 export class AppComponent {
-    
+  message = {
+    content: 'Some content',
+    author: 'Tom'
+  }
 }
